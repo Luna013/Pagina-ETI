66999479d74d695eec381db23a3d7a6abf4ee09f
Ezequiel Luna <hipergamesyt@gmail.com>
2026-06-15 22:48:37 -0300

    Primera version del proyecto ETI

---
+++ app.py
@@ -0,0 +1,60 @@
+from flask import Flask, current_app, render_template, request, session, redirect
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import secure_filename
+from modelo import db
+import os
+
+app = Flask(__name__)
+app.config.from_pyfile('config.py')
+app.config['UPLOAD_FOLDER'] = os.path.join(app.root_path, 'uploads')
+os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+
+db.init_app(app)
+
+from gestorBD import GestorBD
+gestor = GestorBD()
+
+@app.route('/')
+def inicio():
+    return render_template('aviso.html', mensaje="Pagina en construccion", tipo="error-1")
+
+@app.route('/enviarTrabajo', methods = ['GET', 'POST'])
+def enviarTrabajo():
+    if request.method =='POST':
+        campos = ['titulo', 'resumen', 'area', 'nombreAutor', 'apellidoAutor', 'email']
+        if not all([request.form.get(c) for c in campos]):
+            resultado = render_template('aviso.html', mensaje="Llene todos los campos...", tipo="error",)
+        else:
+            archivo = request.files.get('archivo')
+            if archivo:
+                nombre_seguro = secure_filename(archivo.filename)
+                archivo.save(os.path.join(app.config['UPLOAD_FOLDER'], nombre_seguro))
+                idCreado = gestor.registrarTrabajo(request.form.get('titulo'),request.form.get('resumen'),request.form.get('area'),request.form.get('nombreAutor'),request.form.get('apellidoAutor'),request.form.get('email'), nombre_seguro)
+                resultado = render_template('aviso.html', mensaje=f"El trabajo ha sido cargado correctamente. Su numero es: {idCreado}", tipo="exito")
+            else:
+                resultado = render_template('aviso.html', mensaje = "Archivo no cargado", tipo="error")          
+    else:
+        resultado = render_template('enviarTrabajo.html')
+    return resultado
+
+@app.route('/asignar')
+def asignar():
+    trabajos_pendientes = gestor.getTrabajosPendientes()
+    for trabajo in trabajos_pendientes:
+        if len(trabajo.asignaciones) < 3:
+            faltantes = 3 - len(trabajo.asignaciones)
+            areabuscada = trabajo.getArea()
+            evaluadores_area = gestor.getEvaluadoresPorArea(areabuscada)
+            ya_asignados = [asignado.evaluador_id for asignado in trabajo.asignaciones]
+            for evaluador in evaluadores_area:
+                if evaluador.id not in ya_asignados and len(evaluador.asignaciones) < evaluador.max_trabajos:
+                    gestor.crearAsignacion(trabajo.id, evaluador.id)
+                    faltantes -=1
+                    if faltantes == 0:
+                        break    
+    return render_template('aviso.html', mensaje="Asignacion Completada", tipo="exito")
+
+if __name__ == '__main__':
+    with app.app_context():
+        gestor.crearBD()
+        app.run(debug=True)
---
+++ gestorBD.py
@@ -0,0 +1,26 @@
+from werkzeug.security import generate_password_hash
+from modelo import db, Organizador, Evaluador, Trabajo, Asignacion
+
+class GestorBD:
+    def crearBD(self):
+        db.create_all()
+
+    def registrarTrabajo(self,tit, res, ar, nombre, apellido, email, arch_nombre):
+        nuevoTrabajo = Trabajo(titulo = tit, resumen = res, area = ar, autor_nombre = nombre, autor_apellido = apellido, autor_email = email, archivo_nombre = arch_nombre)
+        if not nuevoTrabajo.id:
+            db.session.add(nuevoTrabajo)
+            db.session.commit()
+            idCreada = nuevoTrabajo.getID()
+            return idCreada
+
+    def getTrabajosPendientes(self):
+        return Trabajo.query.filter_by(estado="Pendiente").all()
+
+    def getEvaluadoresPorArea(self, area):
+        return Evaluador.query.filter_by(area=area).all()
+
+    def crearAsignacion(self, trabajoId, evaluadorId):
+        nuevoAsignacion = Asignacion(trabajo_id = trabajoId, evaluador_id = evaluadorId)
+        if not nuevoAsignacion.id:
+            db.session.add(nuevoAsignacion)
+            db.session.commit()
